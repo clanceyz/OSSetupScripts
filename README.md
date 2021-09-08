4225f543d8b88c82fb2a7824aeb97b415515a3de
Clancey Z <8512327+clanceyz@users.noreply.github.com>
2021-09-08 15:56:30 +0800

    1. check if ssh key exists first. 2. added pyenv and latest python. 3. compatible with non-root accounts.

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # About
-Scripts for setting up convenient utilities and security features on Operating Systems.
+Scripts for setting up security features and convenient utilities on Operating Systems.
 
 # Features
 * Security
     * ssh
-        * port 60294
+        * change port to 60294
         * PermitRootLogin without-password
         * PasswordAuthentication no
 * Tools
     * vim, curl, wget, tmux, net-tools, git, zsh, rsync, git, gcc, make, openssl, jdk8.
     * oh-my-zsh: https://github.com/ohmyzsh/ohmyzsh.
     * zsh-autosuggestions: https://github.com/zsh-users/zsh-autosuggestions.
-    * Change tmux shorcut from C-b to C-v to resolve conflict with cursor back shortcut of bash/zsh.
+    * Change tmux shortcut from C-b to C-v to resolve conflict with cursor back shortcut of bash/zsh.
 
 # Warning
-* __First of all, setup a ssh key__, otherwise you might couldn't login ssh anymore because of security settings. The easiest way is to use __ssh-copy-id__ command
+* __First of all, setup a ssh key__, otherwise you will no longer be able to log in to ssh because of security settings. The easiest way is to use __ssh-copy-id__ command
 * The ssh port will change to __60294__ for security concern.
 
 # Install
 * Ubuntu
 ```shell script
-curl -fsSL "https://github.com/clanceyz/OSEnvScripts/raw/master/linux_ubuntu.sh" | sh
+curl -fsSL "https://github.com/clanceyz/OSEnvScripts/raw/master/linux_ubuntu.sh" | sudo sh
 ```
--- linux_ubuntu.sh
+++ linux_ubuntu.sh
@@ -1,14 +1,40 @@
+# environment
+_SSH_PORT=60294
+_USER=${USER}
+if [ ! -z ${SUDO_USER} ]; then
+  _USER=${SUDO_USER}
+fi
+
+info() {
+  echo "[+] $1"
+}
+
+err() {
+  echo "[*] $1"
+  exit 1
+}
+
+# checks
+## ssh key
+if [ ! -e "${HOME}/.ssh/authorized_keys" ]; then
+  err "Please set up a ssh key first (~/.ssh/authorized_keys). Otherwise, you will no longer be able to log in to ssh because of security settings."
+fi
+
 # install dependencies
+info "Installing dependencies"
 apt update
-apt-get install -y vim curl wget tmux net-tools git zsh rsync git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev openjdk-8-jdk
+apt-get install -y vim curl wget tmux net-tools git zsh rsync gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev zlib1g-dev openjdk-8-jdk
 
 # reinforce ssh
-sed -i 's/#Port 22/Port 60294/g' /etc/ssh/sshd_config
+info "Setting up ssh reinforcement"
+sed -i "s/#Port 22/Port ${_SSH_PORT}/g" /etc/ssh/sshd_config
 sed -i -E 's/#?PermitRootLogin.*/PermitRootLogin without-password/g' /etc/ssh/sshd_config
 sed -i -E 's/#?PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 service sshd restart
 
-# oh-my-zsh
+# Utilities
+## oh-my-zsh
+info "Installing oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 chsh -s /usr/bin/zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -22,5 +48,21 @@ echo 'alias la="ls -lha"' >> ~/.zshrc
 echo 'alias lt="ls -lht"' >> ~/.zshrc
 echo '' >> ~/.zshrc
 
-# tmux
+## pyenv
+info "Installing pyenv"
+curl https://pyenv.run | bash
+
+info "Installing latest python"
+pyenv install-latest
+
+## tmux
+info "Setting up tmux"
 echo "set-option -g prefix C-v" > ~/.tmux.conf
+
+## finish
+# change owner of directories
+chown -R ${_USER}:${_USER} ${HOME}/.ssh
+
+info "Done"
+
+info "You can now login ssh on new port ${_SSH_PORT}"
